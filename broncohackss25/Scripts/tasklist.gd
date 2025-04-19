d9545c8949b670b49ca8286a6fdce0e27ff125c1
Enhan540 <enhan540@gmail.com>
2025-04-19 15:00:53 -0700

    Removed comment section

--- broncohackss25/Scripts/tasklist.gd
+++ broncohackss25/Scripts/tasklist.gd
@@ -56,64 +56,7 @@ func test_add_tasks(actions): # takes in dictionary of array of Actions (string,
 		print(action, aca_score, soc_score, well_score)
 		if (aca_score != -19 and soc_score != -19 and well_score != -19):
 			add_task(action, aca_score, soc_score, well_score)
-'''
-var action_pool:= {
-		"Walk": Action.new("Walk", {
-		"energy": 5,
-		"time": 1,
-		"academic": 0, 
-		"wellbeing": 5, 
-		"social": 0
-		}),
-		
-		"Friend_Hangout": Action.new("Friend Hangout", {
-		"energy": 15,
-		"time": 3,
-		"academic": -10, 
-		"wellbeing": 10, 
-		"social": 10
-		}),
-		
-		"Homework": Action.new("Homework", {
-		"energy": 10,
-		"time": 2,
-		"academic": 10, 
-		"wellbeing": -5, 
-		"social": -5
-		}),
-		
-		"Chores": Action.new("Chores", {
-		"energy": 20,
-		"time": 2,
-		"academic": -5, 
-		"wellbeing": 5, 
-		"social": -5
-		}),
-		
-		"Brainrot": Action.new("Brainrot", {
-		"energy": 0,
-		"time": 4,
-		"academic": -10, 
-		"wellbeing": 10, 
-		"social": -10
-		}),
-		
-		"Gym": Action.new("Gym", {
-		"energy": 20,
-		"time": 2,
-		"academic": -5, 
-		"wellbeing": 15, 
-		"social": 0
-		}),
-		
-		"Study": Action.new("Study", {
-		"energy": 20,
-		"time": 2,
-		"academic": 15, 
-		"wellbeing": -5, 
-		"social": -10
-		}),
-''' 
+			 
 func clear():
 	while get_child_count() > 0:
 		num_tasks -= 1
