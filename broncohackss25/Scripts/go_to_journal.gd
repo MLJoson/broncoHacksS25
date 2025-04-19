713977313e452e36e3bea7f157956910fd526eb4
Marco Joson <littlemjoson@gmail.com>
2025-04-19 14:09:01 -0700

    added transition

--- broncohackss25/Scripts/go_to_journal.gd
+++ broncohackss25/Scripts/go_to_journal.gd
@@ -1,4 +1,11 @@
 extends TextureButton
 
+#var transition = $Transition
+
 func _on_pressed() -> void:
+	#transition.play("fade_out")
 	get_tree().change_scene_to_file("res://Scenes/journal.tscn")
+
+func _on_transition_animation_finished(anim_name):
+	#get_tree().change_scene_to_file("res://Scenes/journal.tscn")
+	print("")
