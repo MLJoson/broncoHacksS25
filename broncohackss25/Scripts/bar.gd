b714b23508d4fba40f93f3fd4ae1f6ac00f52023
Marco Joson <littlemjoson@gmail.com>
2025-04-19 02:03:17 -0700

    linked playerstatus with progress bar

--- broncohackss25/Scripts/bar.gd
+++ broncohackss25/Scripts/bar.gd
@@ -1,13 +1,34 @@
 extends ProgressBar
 
+#Connect to player_status file
+@export var PlayerStatus: PlayerStatus
+@export var stat_name: String #"academic", "wellbeing", "social" 
+
+#one start
 func _ready() -> void:
-	value = 100
+	update_bar()
+
+
+
+func update_bar():
+	if PlayerStatus == null:
+		return
+	match stat_name:
+		"academic":
+			value = PlayerStatus.academic
+		"social":
+			value = PlayerStatus.social
+		"wellbeing":
+			value = PlayerStatus.wellbeing
+		_:
+			value = 0
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("increase_val"):
 		value += 20
 	if Input.is_action_just_pressed("decrease_val"):
 		value -= 20
+	update_bar()
 
 func _on_value_changed(value: float) -> void:
 	get("theme_override_styles/fill").bg_color = Color.from_hsv(0.3 * (value / 100.0), 0.9, 0.7)
